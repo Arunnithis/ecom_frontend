2f47077490a5ab25d1b200d1a506a92e88afb7d3
Nithyanantham S/LGSIA QE-1(nithyanantham.s@lgepartner.com) <nithyanantham.s@lge.com>
2023-05-16 17:08:42 +0530

    Change push

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,7 +4,9 @@ import { styled } from 'styled-components'
 const Container = styled.div``
 const ProductList = () => {
   return (
-    <Container></Container>
+    <Container>
+
+    </Container>
   )
 }
 
