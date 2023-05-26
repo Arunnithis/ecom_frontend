26aa4ccbb47f19e7e5fab223e017730fe6773578
Nithyanantham S/LGSIA QE-1(nithyanantham.s@lgepartner.com) <nithyanantham.s@lge.com>
2023-05-26 18:14:37 +0530

    Full commit from office

--- src/App.js
+++ src/App.js
@@ -5,11 +5,28 @@ import Login from './pages/Login';
 import Cart from './pages/Cart';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
 
 function App() {
+  const user = false;
   return (
-    <Home />
+    <BrowserRouter>
+      <Routes>
+          <Route  exact path="/" element={<Home />} />
+          <Route path="/register" element={
+              user ? <Navigate to="/" /> : <Register />
+            } />
+          <Route path="/login" element=
+            {
+              user ? <Navigate to="/" /> : <Login />
+            }
+           />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/products/:category" element={<ProductList />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,6 @@ import Categories from '../components/Categories';
 import Products from '../components/Products';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
-import ProductList from './ProductList';
-import Product from './Product';
 const Home = () => {
   return (
     <>
